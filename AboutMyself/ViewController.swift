1cf03ad0da0944599bab9e721247b0de712eee44
VLADISLAV <SIZONOV.95@MAIL.RU>
2020-10-25 18:14:39 +0300

    zcv

--- AboutMyself/ViewController.swift
+++ AboutMyself/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var UserNameTextField: UITextField!
+    
+    @IBOutlet var ForgotNameButton: UIButton!
+    @IBOutlet var ForgotPasswordButton: UIButton!
+    
+    @IBOutlet var PasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ForgotNameButtonPressed() {
+        guard let inputText = UserNameTextField.text, inputText.isEmpty else {
+        showAlert(with: "Oops!", and: "Your login is User")
+            return
+        }
+    }
+    
+    @IBAction func ForgotPasswordButtonPressed() {
+    }
+    
+    
 }
-
+extension ViewController {
+    private func showAlert(with title: String, and message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "ok", style: .default) {_ in self.UserNameTextField.text = "" }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+}
